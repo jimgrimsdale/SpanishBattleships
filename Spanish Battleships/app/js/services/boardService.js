ab03f7015606dcb8e5b828dbdf7bd59bff7ff930
Jim Grimsdale <jim6788@gmail.com>
2014-12-15 10:24:35 +0000

    Spanish battleships app

---
+++ Spanish Battleships/app/js/services/boardService.js
@@ -0,0 +1,132 @@
+battleshipsApp.factory('boardService', function () {
+  var layout,
+    ships;
+
+  function getLayout () {
+    var r, c, layout, newCell;
+    layout = { rows: [] };
+    for(r = 0; r < 6; r++) {
+      row = [];
+      for(c = 0; c < 12; c++) {
+        row.push({ row: r, column: c, colour: 'blue', hasShip: false });
+      }
+      layout.rows.push(row);
+    }
+
+    return layout;
+  }
+
+  function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+  }
+
+  function getRandomOrientation() {
+    var binary = getRandomInt(0, 2);
+    if (binary === 0) {
+      return 'horizontal';
+    }
+    return 'vertical';
+  }
+
+  function getShips() {
+    var allCells = [],
+      cellCount,
+      randomCell,
+      ships = [],
+      shipCoords,
+      ship;
+    angular.forEach(layout.rows, function (row) {
+      angular.forEach(row, function (cell) {
+        allCells.push(cell);
+      });
+    });
+
+    var shipLengths = [2, 3, 4, 4, 5];
+
+    angular.forEach(shipLengths, function(shipLength) {
+      shipPlaced = false;
+      while(!shipPlaced) {
+        shipCoords = placeShip(shipLength, allCells);
+        if(shipCoords !== null) {
+          ship = {
+            coords: shipCoords,
+            sunk: false
+          };
+          ships.push(ship);
+          shipPlaced = true;
+        }
+      }
+    });
+
+    return ships; 
+  }
+
+  function placeShip (shipLength, allCells) {
+    var cellCount = allCells.length,
+      randomCellIndex = getRandomInt(0, cellCount),
+      randomCell = allCells[randomCellIndex],
+      orientation,
+      shipCoords,
+      row,
+      col,
+      i,
+      cell;
+
+    if (randomCell.hasShip)  {
+      return null;
+    }
+
+    orientation = getRandomOrientation();
+    shipCoords = [];
+    row = randomCell.row;
+    col = randomCell.column;
+
+    if (orientation === 'horizontal') {
+      if(col - shipLength < -1) {
+        return null;
+      } else {        
+        for (i = col - shipLength + 1; i < col; i++) {
+          cell = layout.rows[row][i];
+          if(cell.hasShip) {
+            return null;
+          }
+        }
+        for (i = col - shipLength + 1; i < col + 1; i++) {
+          layout.rows[row][i].hasShip = true;
+          shipCoords.push({ x: i, y: row });
+        }
+        return shipCoords;
+      }
+    } else {
+      if (row - shipLength < -1) {
+        return null;
+      } else {
+        for (i = row - shipLength + 1; i < row; i++) {
+          cell = layout.rows[i][col];
+          if(cell.hasShip) {
+            return null;
+          }
+        }
+        for (i = row - shipLength + 1; i < row + 1; i++) {
+          layout.rows[i][col].hasShip = true;
+          shipCoords.push({ x: col, y: i });
+        }
+        return shipCoords;
+      }
+    }
+
+    return null;
+  }
+
+
+  return {
+    layout: function () {
+      layout = getLayout();
+      return layout;
+    },
+    ships: function () {
+      ships = getShips();
+      return ships;
+    }
+  }
+});
---
+++ Spanish Battleships/app/js/services/dictionaryService.js
@@ -0,0 +1,103 @@
+﻿battleshipsApp.factory('dictionaryService', function () {
+  var verbDictionary = [
+    {
+      verb: 'ser',
+      present: ['soy', 'eres', 'es', 'somos', 'sois', 'son'],
+      imperfect: ['yo era', 'eras', 'era', 'éramos', 'érais', 'eran'],
+      preterite: ['fui', 'fuiste', 'fue', 'fuimos', 'fuisteis', 'fueron']
+    },
+    {
+      verb: 'estar',
+      present: ['estoy', 'estás', 'está', 'estamos', 'estáis', 'estan'],
+      imperfect: ['yo estaba', 'estabas', 'estaba', 'estábamos', 'estabais', 'estaban'],
+      preterite: ['estuve', 'estuviste', 'estuvo', 'estuvimos', 'estuvisteis', 'estuvieron']
+    },
+    {
+      verb: 'cantar',
+      present: ['canto', 'cantas', 'canta', 'cantamos', 'cantáis', 'cantan'],
+      imperfect: ['yo cantaba', 'cantabas', 'cantaba', 'cantábamos', 'cantabais', 'cantaban'],
+      preterite: ['canté', 'cantaste', 'cantó', 'cantamos', 'cantasteis', 'cantaron']
+    },
+    {
+      verb: 'leer',
+      present: ['leo', 'lees', 'lee', 'leemos', 'leéis', 'leen'],
+      imperfect: ['yo leía', 'leías', 'leía', 'leíamos', 'leíais', 'leían'],
+      preterite: ['leí', 'leiste', 'leyó', 'leímos', 'leisteis', 'leyeron']
+    },
+    {
+      verb: 'poder',
+      present: ['puedo', 'puedes', 'puede', 'podemos', 'podéis', 'pueden'],
+      imperfect: ['yo podía', 'podías', 'podía', 'podíamos', 'podíais', 'podían'],
+      preterite: ['pude', 'pudiste', 'pudo', 'pudimos', 'pudisteis', 'pudieron']
+    },
+    { 
+      verb: 'nadar',
+      present: ['nado', 'nadas', 'nada', 'nadamos', 'nadáis', 'nadan'],
+      imperfect: ['yo nadaba', 'nadabas', 'nadaba', 'nadábamos', 'nadabais', 'nadaban'],
+      preterite: ['nadé', 'nadaste', 'nadó', 'nadamos', 'nadasteis', 'nadaron']
+    },
+    { 
+      verb: 'estudiar',
+      present: ['estudio', 'estudias', 'estudia', 'estudiamos', 'estudiáis', 'estudian'],
+      imperfect: ['yo estudiaba', 'estudiabas', 'estudiaba', 'estudiábamos', 'estudiabais', 'estudiaban'],
+      preterite: ['estudié', 'estudiaste', 'estudió', 'estudiamos', 'estudiasteis', 'estudiaron']
+    },
+    { 
+      verb: 'lavar',
+      present: ['lavo', 'lavas', 'lava', 'lavamos', 'laváis', 'lavan'],
+      imperfect: ['yo lavaba', 'lavabas', 'lavaba', 'lavábamos', 'lavabais', 'lavaban'],
+      preterite: ['lavé', 'lavaste', 'lavó', 'lavamos', 'lavasteis', 'lavaron']
+    },
+    { 
+      verb: 'limpiar',
+      present: ['limpio', 'limpias', 'limpia', 'limpiamos', 'limpiáis', 'limpian'],
+      imperfect: ['yo limpiaba', 'limpiabas', 'limpiaba', 'limpiábamos', 'limpiabais', 'limpiaban'],
+      preterite: ['limpié', 'limpiaste', 'limpió', 'limpiamos', 'limpiasteis', 'limpiaron']
+    },
+    { 
+      verb: 'jugar',
+      present: ['juego', 'juegas', 'juega', 'jugamos', 'jugáis', 'juegan'],
+      imperfect: ['yo jugaba', 'jugabas', 'jugaba', 'jugábamos', 'jugabais', 'jugaban'],
+      preterite: ['jugué', 'jugaste', 'jugó', 'jugamos', 'jugasteis', 'jugaron']
+    },
+    { 
+      verb: 'tocar',
+      present: ['toco', 'tocas', 'toca', 'tocamos', 'tocáis', 'tocan'],
+      imperfect: ['yo tocaba', 'tocabas', 'tocaba', 'tocábamos', 'tocabais', 'tocaban'],
+      preterite: ['toqué', 'tocaste', 'tocó', 'tocamos', 'tocasteis', 'tocaron']
+    },
+    { 
+      verb: 'trabajar',
+      present: ['trabajo', 'trabajas', 'trabaja', 'trabajamos', 'trabajáis', 'trabajan'],
+      imperfect: ['yo trabajaba', 'trabajabas', 'trabajaba', 'trabajábamos', 'trabajabais', 'trabajaban'],
+      preterite: ['trabajé', 'trabajaste', 'trabajó', 'trabajamos', 'trabajasteis', 'trabajaron']
+    }
+  ];
+
+  var providedVerbs = [];
+
+  return {
+    get12Verbs: function get12Verbs (tense) {
+      var verbsToReturn = [];
+      providedVerbs = [];
+      angular.forEach(verbDictionary, function (entry) {
+        switch (tense) {
+          case "present": verbsToReturn.push({ verb: entry.verb, verbs: entry.present });
+            providedVerbs.push.apply(providedVerbs, entry[tense]);
+            break;
+          case "imperfect": verbsToReturn.push({ verb: entry.verb, verbs: entry.imperfect });
+            providedVerbs.push.apply(providedVerbs, entry[tense]);
+            break;
+          case "preterite": verbsToReturn.push({ verb: entry.verb, verbs: entry.preterite });
+            providedVerbs.push.apply(providedVerbs, entry[tense]);
+            break;
+        }        
+      });
+      // todo: getRandom 12 verbs
+      return verbsToReturn;
+    },
+    getProvidedVerbs: function () {
+      return providedVerbs;
+    }
+  }
+});
